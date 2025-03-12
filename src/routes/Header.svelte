974507dfe4c23308518cfd133dbf47b01aa57d92
kamatealif <kamatealip1@gmail.com>
2025-03-12 12:20:12 +0530

    iron man

---
+++ src/routes/Header.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    let { name }:{
+        name: string;
+    } = $props();
+</script>
+
+<h1>
+    Hello {name }
+</h1>
+<h2>
+    {name.replace("l","r")}
+</h2>
+
+<style>
+
+</style>
