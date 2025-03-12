7038c365357be2ebdf594d22352f4bb41bc6452d
kamatealif <kamatealip1@gmail.com>
2025-03-12 12:27:36 +0530

    events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,25 @@
-<script>
+<script lang='ts'>
     import Header from "./Header.svelte";
+    let name = $state('Scott')
 
-    let name = 'Alif'
+    let status: "OPEN" | "CLOSED" = $state("OPEN")
+
+    function toggle(){
+        status = status === "OPEN" ? "CLOSED" : "OPEN";
+    }
 </script>
 
 <Header {name}/>
+
+<input type="text" bind:value={name} />
+
+
+<p>The store is now {status}</p>
+<button onclick={toggle}>Toggle Status</button>
+
+
+<style>
+    input{
+        padding: 4px 8px;
+    }
+</style>
