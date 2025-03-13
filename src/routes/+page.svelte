31692924018c39e66cfea9c0089bce0657f46bd6
kamatealif <kamatealip1@gmail.com>
2025-03-13 11:34:50 +0530

    learn derived states

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang='ts'>
     import Header from "./Header.svelte";
-    let name = $state('Scott')
+    let name = $state('')
 
     let status: "OPEN" | "CLOSED" = $state("OPEN")
+
+    let full_name = $derived(name + " " + "Smith")
 
     // function toggle(){
     //     status = status === "OPEN" ? "CLOSED" : "OPEN";
@@ -13,13 +15,14 @@
     }
 </script>
 
-<Header {name}/>
+<Header {name} fake_name="lallu"/>
 
 <input type="text" bind:value={name} />
 
 
 <p>The store is now {status}</p>
 <button onclick={onclick}>Toggle Status</button>
+<h2>{full_name}</h2>
 
 <!-- COOL STUFF -->
 <!-- <button {onclick}>Toggle Status</button>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,9 @@
 <script lang='ts'>
-    let { name }:{
+    let { name,
+        fake_name = null
+     }:{
         name: string;
+        fake_name?: string | null
     } = $props();
 </script>
 
@@ -8,8 +11,9 @@
     Hello {name }
 </h1>
 <h2>
-    {name.replace("l","r")}
+    {name.replaceAll("l","r")}
 </h2>
+<p>Fake name : {fake_name}</p>
 
 <style>
 
